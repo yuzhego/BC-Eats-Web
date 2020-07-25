03a8e0f0bfe11d7c2e4527343f93647df9f2b4b6
Patrick Render <rendep@uw.edu>
2020-07-24 17:32:12 -0700

    set up all routing for current pages

--- functions/index.js
+++ functions/index.js
@@ -6,11 +6,27 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 
 app.get('/', (request, response) => {
-    response.render('index', {"title": "Facts"});
+    response.render('index');
 });
 
 app.get('/home', (request, response) => {
     response.render('home');
+});
+
+app.get('/feed', (request, response) => {
+    response.render('feed');
+});
+
+app.get('/newpost', (request, response) => {
+    response.render('newpost');
+});
+
+app.get('/editpost', (request, response) => {
+    response.render('editpost');
+});
+
+app.get('/loginoptions', (request, response) => {
+    response.render('loginoptions');
 });
 
 exports.app = functions.https.onRequest(app)
